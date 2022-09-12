cace6088bf59248fcb566f0c1b2c272e957538e2
leesul <suerish.e@gmail.com>
2022-09-12 18:58:31 +0900

    배너 수정

---
+++ src/components/Banner/BottomBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bottom-banner banner">
+    <b-container class="h-100">
+      <b-row class="h-100 m-auto align-items-center">
+        <b-col class="text-center text-lg-start" lg="4">
+          <p class="text-white text-20 m-0">
+            우리가게 마케팅 고민 <br />오늘의 픽이 해결해드려요.
+          </p>
+        </b-col>
+        <b-col class="bg-img d-none d-lg-block"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopBanner",
+};
+</script>
+
+<style></style>

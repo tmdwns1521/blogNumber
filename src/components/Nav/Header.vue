a954341259b1f5b55b1b140faafd312c5b158f81
leesul <55569192+helloleesul@users.noreply.github.com>
2022-09-13 13:39:53 +0900

    헤더 생성

--- src/components/Nav/Header.vue
+++ src/components/Nav/Header.vue
@@ -3,13 +3,23 @@
     <TopBanner :show="topBanner" />
     <header class="shadow-sm py-3 position-relative">
       <b-container>
-        <b-navbar-brand href="/">
-          <img
-            src="@/assets/images/logo.svg"
-            alt="오늘의 픽 로고"
-            :style="{ height: '50px' }"
-          />
-        </b-navbar-brand>
+        <div class="justify-content-between align-items-center d-flex">
+          <b-navbar-brand href="/">
+            <img
+              src="@/assets/images/logo.svg"
+              alt="오늘의 픽 로고"
+              :style="{ height: '50px' }"
+            />
+          </b-navbar-brand>
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-avatar></b-avatar>
+              <span>회원이름</span>
+            </template>
+            <b-dropdown-item href="/manage">관리페이지</b-dropdown-item>
+            <b-dropdown-item href="#">로그아웃</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </div>
       </b-container>
     </header>
   </div>

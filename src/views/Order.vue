354606ae7ede47c31baf5eada8a70a67bbf10e75
leesul <55569192+helloleesul@users.noreply.github.com>
2022-09-19 16:28:31 +0900

    주문관리 화면 그릴 예정

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,15 +1,405 @@
 <template>
-  <div class="order-page">
+  <div class="sales-page">
     <b-row class="justify-content-between align-items-center mb-3">
       <b-col>
         <h1 class="text-25 m-0">주문관리</h1>
       </b-col>
     </b-row>
+    <div style="max-width: 1500px"></div>
+    <b-tabs content-class="p-4" no-fade>
+      <b-tab title="메인" active>
+        <div class="justify-content-between align-items-center mb-3 d-flex">
+          <h2 class="fw-900 m-0">DATA</h2>
+          <b-row class="justify-content-end align-items-center">
+            <!-- 딜력 검색 Start Date -->
+            <b-col cols="3">
+              <b-input-group>
+                <b-form-input
+                  id="schStartDate"
+                  v-model="schStartDate"
+                  type="text"
+                  placeholder="시작일"
+                  autocomplete="off"
+                ></b-form-input>
+                <b-input-group-append>
+                  <b-form-datepicker
+                    v-model="schStartDate"
+                    button-only
+                    right
+                    locale="ko"
+                    aria-controls="schStartDate"
+                    hide-header
+                  ></b-form-datepicker>
+                </b-input-group-append>
+              </b-input-group>
+            </b-col>
+            <!-- 딜력 검색 End Date -->
+            <b-col cols="3">
+              <b-input-group>
+                <b-form-input
+                  id="schEndDate"
+                  v-model="schEndDate"
+                  type="text"
+                  placeholder="종료일"
+                  autocomplete="off"
+                ></b-form-input>
+                <b-input-group-append>
+                  <b-form-datepicker
+                    v-model="schEndDate"
+                    button-only
+                    right
+                    locale="ko"
+                    aria-controls="schEndDate"
+                    hide-header
+                  ></b-form-datepicker>
+                </b-input-group-append>
+              </b-input-group>
+            </b-col>
+            <!-- 검색어 -->
+            <b-col cols="5">
+              <b-form-group v-slot="{ ariaDescribedby }">
+                <b-input-group>
+                  <b-form-input
+                    class="mx-2"
+                    id="filter-input"
+                    v-model="filter"
+                    :aria-describedby="ariaDescribedby"
+                    type="search"
+                    placeholder="검색어"
+                  ></b-form-input>
+                  <b-input-group-append>
+                    <b-btn
+                      @click="filter = null"
+                      :disabled="!filter"
+                      variant="outline-dark"
+                      style="
+                        border-top-left-radius: 0;
+                        border-bottom-left-radius: 0;
+                      "
+                      >초기화</b-btn
+                    >
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </div>
+        <b-table
+          head-variant="dark"
+          bordered
+          responsive
+          hover
+          selectable
+          select-mode="single"
+          @row-selected="onRowSelected"
+          :sticky-header="true"
+          :items="salesItems"
+          :fields="fields"
+          class="dataTable"
+          :filter="filter"
+          ref="selectableTable"
+          show-empty
+          emptyFilteredText="찾으시는 검색어와 일치하는 정보가 없습니다."
+          emptyText="데이터 정보가 없습니다."
+        >
+          <!-- No. -->
+          <template #cell(index)="row">
+            {{ row.item.index + 1 }}
+          </template>
+          <!-- 계약번호 -->
+          <template #cell(ContractNumber)="row">
+            {{ dateFormat1(row.item.ContractNumber) }}
+          </template>
+          <!-- 결제일 -->
+          <template #cell(payDate)="row">
+            {{ dateFormat2(row.item.ContractNumber) }}
+          </template>
+          <!-- 시작일 -->
+          <template #cell(trfficDataCreated)="row">
+            {{ dateFormat2(row.item.trfficData.created_at) }}
+          </template>
+          <!-- 종료일 -->
+          <template #cell(trfficDataExpiration)="row">
+            {{ dateFormat2(row.item.trfficData.cexpiration_date) }}
+          </template>
+          <!-- 실시간트래픽 -->
+          <template #cell(trfficDataTodayCount)="row">
+            {{ row.item.trfficData.today_remain_count }}
+          </template>
+          <template #cell(AmountOfPayment)="row">
+            {{ numberToString(parseInt(row.item.AmountOfPayment)) }}원
+          </template>
+          <!-- 디자인체크 -->
+          <template #cell(designChk)="row">
+            <div class="text-center">
+              <font-awesome-icon
+                v-if="row.item.design"
+                class="fa-xl text-success"
+                icon="fa-solid fa-circle-check"
+              />
+              <font-awesome-icon
+                v-else
+                style="color: #ced4da"
+                class="fa-xl"
+                icon="fa-solid fa-circle-xmark"
+              />
+            </div>
+          </template>
+          <!-- 트래픽체크 -->
+          <template #cell(trafficChk)="row">
+            <div class="text-center">
+              <font-awesome-icon
+                v-if="row.item.traffic"
+                class="fa-xl text-success"
+                icon="fa-solid fa-circle-check"
+              />
+              <font-awesome-icon
+                v-else
+                style="color: #ced4da"
+                class="fa-xl"
+                icon="fa-solid fa-circle-xmark"
+              />
+            </div>
+          </template>
+          <!-- 확인체크 -->
+          <template #cell(doneChk)="row">
+            <div class="text-center">
+              <font-awesome-icon
+                v-if="row.item.doneChk"
+                class="fa-xl text-success"
+                icon="fa-solid fa-circle-check"
+              />
+              <font-awesome-icon
+                v-else
+                style="color: #ced4da"
+                class="fa-xl"
+                icon="fa-solid fa-circle-xmark"
+              />
+            </div>
+          </template>
+        </b-table>
+      </b-tab>
+      <b-tab title="완료">완료</b-tab>
+      <b-tab title="취소">취소</b-tab>
+      <b-tab title="기타상품">기타상품</b-tab>
+    </b-tabs>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Sales",
+  data() {
+    return {
+      fields: [
+        {
+          key: "index",
+          label: "No",
+          // isRowHeader: true,
+          // sortable: true,
+          variant: "secondary",
+          thClass: "",
+          // filterByFormatted: true,
+        },
+        {
+          key: "ContractNumber",
+          label: "계약번호",
+          thClass: "table-secondary",
+        },
+        {
+          key: "manager",
+          label: "담당자",
+          thClass: "table-secondary",
+        },
+        {
+          key: "businessName",
+          label: "상호명",
+          stickyColumn: true,
+          thClass: "table-secondary",
+        },
+        {
+          key: "owner",
+          label: "대표자",
+          thClass: "table-secondary",
+        },
+        {
+          key: "phone",
+          label: "연락처",
+          thClass: "table-secondary",
+        },
+        {
+          key: "CompanyNumber",
+          label: "사업자등록번호",
+          thClass: "table-secondary",
+        },
+        {
+          key: "Email",
+          label: "이메일",
+          thClass: "table-secondary",
+        },
+        {
+          key: "address",
+          label: "주소",
+          thClass: "table-secondary",
+        },
+        {
+          key: "trfficData.blog",
+          label: "URL",
+          thClass: "table-secondary",
+        },
+        {
+          key: "blogId",
+          label: "아이디",
+          thClass: "table-secondary",
+        },
+        {
+          key: "blogPw",
+          label: "비밀번호",
+          thClass: "table-secondary",
+        },
+        {
+          key: "contractProduct",
+          label: "계약상품",
+          thClass: "table-secondary",
+        },
+        {
+          key: "trfficDataTodayCount",
+          label: "실시간트래픽",
+          thClass: "table-secondary",
+        },
+        {
+          key: "designChk",
+          label: "디자인",
+          thClass: "table-secondary",
+        },
+        {
+          key: "trafficChk",
+          label: "트래픽",
+          thClass: "table-secondary",
+        },
+        {
+          key: "trfficDataCreated",
+          label: "시작일",
+          thClass: "table-secondary",
+        },
+        {
+          key: "AmountOfPayment",
+          label: "결제금액",
+          thClass: "table-secondary",
+        },
+        {
+          key: "doneChk",
+          label: "확인",
+          thClass: "table-secondary",
+        },
+        {
+          key: "Term",
+          label: "계약기간",
+          thClass: "table-secondary",
+        },
+        {
+          key: "trfficDataExpiration",
+          label: "종료일",
+          thClass: "table-secondary",
+        },
+        {
+          key: "cardData.cardholder",
+          label: "카드소유자",
+          thClass: "table-secondary",
+        },
+        {
+          key: "cardData.creditCardCompany",
+          label: "카드사",
+          thClass: "table-secondary",
+        },
+        {
+          key: "cardData.creditCardNumber",
+          label: "카드번호",
+          thClass: "table-secondary",
+        },
+        {
+          key: "cardData.CardValidityPeriod",
+          label: "카드유효기간",
+          thClass: "table-secondary",
+        },
+        {
+          key: "ApprovalNumber",
+          label: "카드승인번호",
+          thClass: "table-secondary",
+        },
+        {
+          key: "installmentMonth",
+          label: "할부개월",
+          thClass: "table-secondary",
+        },
+        {
+          key: "payDate",
+          label: "결제일",
+          thClass: "table-secondary",
+        },
+        {
+          key: "Note",
+          label: "비고",
+          thClass: "table-secondary",
+        },
+      ],
+      salesItems: null,
+      filter: "",
+      currentData: {},
+      newData: {
+        manager: null,
+        businessName: null,
+        owner: null,
+        trfficData: {},
+        blogId: null,
+        blogPw: null,
+        CompanyNumber: null,
+        phone: null,
+        Email: null,
+        address: null,
+        cardData: {},
+        AmountOfPayment: null,
+        ApprovalNumber: null,
+        installmentMonth: null,
+        Term: null,
+        contractProduct: null,
+        Note: null,
+      },
+      addTag: false,
+      updateTag: false,
+      empty: "-",
+      schStartDate: "",
+      schEndDate: "",
+    };
+  },
+  methods: {
+    // 신규등록 완료
+    addData() {
+      this.addTag = false;
+    },
+    // 수정 완료
+    updateData() {
+      this.updateTag = false;
+    },
+    onRowSelected(items) {
+      this.addTag = false;
+      items = items[0];
+      this.currentData = { ...items };
+    },
+    async salesData() {
+      const data = await this.$axios.get("/api/salesData");
+      console.log(data.data);
+      this.salesItems = data.data;
+      this.salesItems.forEach((el, index) => {
+        this.$set(el, "index", index);
+      });
+    },
+  },
+  mounted() {
+    this.salesData();
+  },
+  computed: {},
+};
 </script>
 
 <style></style>

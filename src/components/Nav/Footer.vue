c02a21c65b9bfd150eac68e0796c85da2bf46063
leesul <55569192+helloleesul@users.noreply.github.com>
2022-10-07 18:24:12 +0900

    하단 상호명

--- src/components/Nav/Footer.vue
+++ src/components/Nav/Footer.vue
@@ -2,9 +2,12 @@
   <div class="footer">
     <!-- PC -->
     <b-container class="d-none d-lg-block py-5">
-      <div class="d-flex align-items-center justify-content-between mb-4">
-        <h1 class="text-25 fw-900 m-0">오늘의 픽</h1>
-        <!-- <b-nav-item-dropdown right v-if="$store.getters.isLogin">
+      <b-row class="align-items-center">
+        <b-col cols="2"><h3 class="fw-900">엔에스엠(NSM)</h3></b-col>
+        <b-col>
+          <div class="d-flex align-items-center justify-content-between mb-4">
+            <h1 class="text-25 fw-900 m-0">오늘의 픽</h1>
+            <!-- <b-nav-item-dropdown right v-if="$store.getters.isLogin">
           <template #button-content>
             <b-avatar></b-avatar>
             <span>{{ $store.state.serviceId }}</span>
@@ -12,34 +15,36 @@
           <b-dropdown-item href="/manage">관리페이지</b-dropdown-item>
           <b-dropdown-item href="#" @click="logout()">로그아웃</b-dropdown-item>
         </b-nav-item-dropdown> -->
-        <b-btn
-          v-if="$store.getters.isLogin === false"
-          @click="login()"
-          variant="link"
-          class="text-secondary text-decoration-none"
-        >
-          관리자
-        </b-btn>
-      </div>
-      <div class="d-flex" style="gap: 20px">
-        <p>
-          <span class="fw-900">대표</span>
-          민경재
-        </p>
-        <p>
-          <span class="fw-900">상호</span>
-          엔에스엠(NSM)
-        </p>
-        <p>
-          <span class="fw-900">사업자등록번호</span>
-          417-16-97188
-        </p>
-        <p>
-          <span class="fw-900">주소</span>
-          서울시 영등포구 경인로 775, 1동 215호(에이스하이테크시티)
-        </p>
-      </div>
-      <span>© 오늘의 픽. All rights reserved.</span>
+            <b-btn
+              v-if="$store.getters.isLogin === false"
+              @click="login()"
+              variant="link"
+              class="text-secondary text-decoration-none"
+            >
+              관리자
+            </b-btn>
+          </div>
+          <div class="d-flex" style="gap: 20px">
+            <p>
+              <span class="fw-900">대표</span>
+              민경재
+            </p>
+            <p>
+              <span class="fw-900">상호</span>
+              엔에스엠(NSM)
+            </p>
+            <p>
+              <span class="fw-900">사업자등록번호</span>
+              417-16-97188
+            </p>
+            <p>
+              <span class="fw-900">주소</span>
+              서울시 영등포구 경인로 775, 1동 215호(에이스하이테크시티)
+            </p>
+          </div>
+          <span>© 오늘의 픽. All rights reserved.</span>
+        </b-col>
+      </b-row>
     </b-container>
     <!-- Mobile -->
     <b-container class="d-lg-none d-block py-3">

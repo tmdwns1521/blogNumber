0e4ff2a2c6450d8f72b87376da5559ca23a2678e
leesul <55569192+helloleesul@users.noreply.github.com>
2022-10-07 12:38:15 +0900

    데이터 헤더 클릭 시 ellipsis

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -209,9 +209,31 @@
             {{ row.item.blogId }}@naver.com
           </span>
         </template>
+        <!-- 주소 -->
+        <template #head(address)="data">
+          <div @click="isEllipsis = !isEllipsis" class="cursor-pointer">
+            {{ data.label }}
+          </div>
+        </template>
+        <template #cell(address)="row">
+          <div
+            :class="{ ellipsis: isEllipsis }"
+            v-clipboard:copy="row.item.address"
+            v-clipboard:success="onCopy"
+            v-clipboard:error="onError"
+          >
+            {{ row.item.address }}
+          </div>
+        </template>
         <!-- URL -->
+        <template #head(url)="data">
+          <div @click="isEllipsis = !isEllipsis" class="cursor-pointer">
+            {{ data.label }}
+          </div>
+        </template>
         <template #cell(url)="row">
           <a
+            :class="{ ellipsis: isEllipsis }"
             :href="'https://m.blog.naver.com/' + row.item.blogId"
             target="_blank"
             v-clipboard:copy="'https://blog.naver.com/' + row.item.blogId"
@@ -281,6 +303,7 @@ export default {
   props: ["currentData", "salesItems", "orderItems"],
   data() {
     return {
+      isEllipsis: false,
       fields: [
         {
           key: "index",
@@ -445,43 +468,6 @@ export default {
     onRowSelected(items) {
       this.$emit("onRowSelected", items);
     },
-  },
-  mounted: function () {
-    var thElm;
-    var startOffset;
-
-    Array.prototype.forEach.call(
-      document.querySelectorAll("table th"),
-      function (th) {
-        th.style.position = "relative";
-        // th.style.display = "block";
-
-        var grip = document.createElement("div");
-        grip.innerHTML = "&nbsp;";
-        grip.style.top = 0;
-        grip.style.right = 0;
-        grip.style.bottom = 0;
-        grip.style.width = "5px";
-        grip.style.position = "absolute";
-        grip.style.cursor = "col-resize";
-        grip.addEventListener("mousedown", function (e) {
-          thElm = th;
-          startOffset = th.offsetWidth - e.pageX;
-        });
-
-        th.appendChild(grip);
-      }
-    );
-
-    document.addEventListener("mousemove", function (e) {
-      if (thElm) {
-        thElm.style.width = startOffset + e.pageX + "px";
-      }
-    });
-
-    document.addEventListener("mouseup", function () {
-      thElm = undefined;
-    });
   },
 };
 </script>
